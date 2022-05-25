7b2274b47e3a9cc23cbb0c3b7eb9e3a778a64a29
mmaia <mmaia@uolinc.com>
2022-05-25 09:26:19 -0300

    Iniciando projeto

---
+++ src/main/java/com/desenvolvimentoApi/desenvolvimentoApi/DesenvolvimentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimentoApi.desenvolvimentoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvimentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvimentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desenvolvimentoApi/desenvolvimentoApi/DesenvolvimentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvimentoApi.desenvolvimentoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
