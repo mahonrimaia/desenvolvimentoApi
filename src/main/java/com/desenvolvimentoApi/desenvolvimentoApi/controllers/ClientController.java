463888d3425d358b8382d1cbce6a7314e582facc
queziaqueiroz <quezia.queiroz@moip.com.br>
2022-06-06 10:46:58 -0300

    Criação do Get do ClientController

--- src/main/java/com/desenvolvimentoApi/desenvolvimentoApi/controllers/ClientController.java
+++ src/main/java/com/desenvolvimentoApi/desenvolvimentoApi/controllers/ClientController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping ("/client")
 public class ClientController {
@@ -15,18 +18,29 @@ public class ClientController {
         this.repository = repository;
     }
 
-    @GetMapping("/customer")
-    public String hello(){
-        return "Bem-vindo";
+    @GetMapping ("/customer/all")
+   public ResponseEntity showClientList(){
+       List<ClientModel> response = repository.findAll();
+       if (response != null){
+           return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+       }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não existe cleinte cadastrado");
     }
-
+    @GetMapping("/customer/{id}")
+    public ResponseEntity showClienteId(@PathVariable("id") long id){
+        Optional<ClientModel> customer = repository.findById(id);
+        if (customer.isPresent()){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(customer);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id não encontrado");
+    }
     @PostMapping ("/new")
     public ResponseEntity createNewClient(@RequestBody ClientModel client){
         Long response = repository.save(client).getId();
         if (response != null){
-            return ResponseEntity.accepted().body(client);
+            return ResponseEntity.status(HttpStatus.CREATED).body(client);
         }
-        return ResponseEntity.ok(HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verifique os dados informado");
     }
 
 }
